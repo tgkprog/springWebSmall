23eba5e53eaa09077e63530f071bb5e837e098cd
Tushar Kapila <tgkprog@gmail.com>
2018-03-11 22:39:52 +0530

    initial

---
+++ src/main/java/com/vfr/Dsf/DsfApplication.java
@@ -0,0 +1,12 @@
+package com.vfr.Dsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsfApplication.class, args);
+	}
+}
